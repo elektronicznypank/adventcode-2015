4ad7ec6e119a67fb13d7bd6f10a0c8611907cfd4
elektronicznypank <dizelpauer115@gmail.com>
2023-05-21 15:43:50 +0200

    Code cleaning and fixind, completed test file

---
+++ day2/test_adventcode2015_day2.py
@@ -0,0 +1,36 @@
+# This script is to be run with pytest
+
+from adventcode2015_day2 import count_paper_dimension
+
+test_cases: list[dict] = [
+    {
+        'input1': ['1x1x10'],
+        'input2': ['2x3x4'],
+        'input3': ['1x1x2'],
+        'input4': ['1x2x1'],
+        'input5': ['2x1x1'],
+        'input6': ['1x1x1']
+    },
+    {
+        'input1_expected': 43,
+        'input2_expected': 58,
+        'input3_expected': 11,
+        'input4_expected': 11,
+        'input5_expected': 11,
+        'input6_expected': 7
+
+    }
+]
+
+
+def test_count_paper_dimension():
+    assert count_paper_dimension(test_cases[0]['input1']) == test_cases[1]['input1_expected']
+    assert count_paper_dimension(test_cases[0]['input2']) == test_cases[1]['input2_expected']
+    assert count_paper_dimension(test_cases[0]['input3']) == test_cases[1]['input3_expected']
+    assert count_paper_dimension(test_cases[0]['input4']) == test_cases[1]['input4_expected']
+    assert count_paper_dimension(test_cases[0]['input5']) == test_cases[1]['input5_expected']
+    assert count_paper_dimension(test_cases[0]['input6']) == test_cases[1]['input6_expected']
+
+
+if __name__ == '__main__':
+    print(test_count_paper_dimension())
