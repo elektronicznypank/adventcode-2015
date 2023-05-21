0850797e733518e87c59a077cdb5d3a87f28cbe9
elektronicznypank <dizelpauer115@gmail.com>
2023-05-21 10:49:21 +0200

    Modified alpha version, added test file

--- day2/adventcode2015-day2.py
+++ day2/adventcode2015-day2.py
@@ -1,18 +1,17 @@
-with open('input.txt', 'r') as paper_dimensions:
-    paper_list: list = paper_dimensions.readlines()
+with open('input.txt', 'r') as paper_dimensions_data:
+    wrapping_paper: list = paper_dimensions_data.readlines()
 
 
-def count_paper_dimension(final_dimension=0):
-    for i in range(len(paper_list)):
-        paper_list[i] = paper_list[i].replace('\n', '')
-        q, w, e = paper_list[i].split('x')
-        q, w, e = int(q), int(w), int(e)
-        minimum: int = min(q, w, e) ** 2
-        length = 2 * q * w
-        width = 2 * w * e
-        height = 2 * q * e
-        final_dimension += length + width + height + minimum
+def count_paper_dimension(final_dimension=0) -> int:
+    for i in range(len(wrapping_paper)):
+        wrapping_paper[i] = wrapping_paper[i].replace('\n', '')
+        l, w, h = wrapping_paper[i].split('x')
+        l, w, h = int(l), int(w), int(h)
+        extra_paper: int = min(l, w, h) ** 2
+        present_area: int = 2 * (l * w + w * h + l * h)
+        final_dimension += present_area + extra_paper
     return final_dimension
 
 
-print(count_paper_dimension())
+if __name__ == '__main__':
+    print(count_paper_dimension())
