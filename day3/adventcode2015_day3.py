1e6d9e876044c951b92523ebd95b8a8c8be85563
elektronicznypank <dizelpauer115@gmail.com>
2023-05-22 21:55:05 +0200

    PEP fixes

--- day3/adventcode2015_day3.py
+++ day3/adventcode2015_day3.py
@@ -5,8 +5,10 @@ with open('input.txt') as houses:
 
 
 def count_gifted_houses_by_santa(houses_input: str) -> int:
+
     houses_coordinates_santa_alone: list = [0, 0]
     houses_coordinates_matrix_santa: set = {(0, 0)}
+
     for i in houses_input:
         houses_coordinates_santa_alone = list(houses_coordinates_santa_alone)
         if i == '^':
@@ -17,20 +19,27 @@ def count_gifted_houses_by_santa(houses_input: str) -> int:
             houses_coordinates_santa_alone[1] += 1
         elif i == '<':
             houses_coordinates_santa_alone[1] -= 1
+
         houses_coordinates_santa_alone: tuple = tuple(houses_coordinates_santa_alone)
         houses_coordinates_matrix_santa.add(houses_coordinates_santa_alone)
+
     return len(houses_coordinates_matrix_santa)
 
 
 # part 2
 
+
 def count_gifted_houses_by_santa_and_robot(houses_input: str) -> int:
+
     houses_coordinates_santa: list = [0, 0]
     houses_coordinates_robot: list = [0, 0]
     houses_coordinates_matrix_santa_and_robot: set = {(0, 0)}
+
     for j, k in enumerate(houses_input):
+
         houses_coordinates_santa: list = list(houses_coordinates_santa)
         houses_coordinates_robot: list = list(houses_coordinates_robot)
+
         if j % 2 == 0:
             if k == '^':
                 houses_coordinates_santa[0] += 1
@@ -49,10 +58,13 @@ def count_gifted_houses_by_santa_and_robot(houses_input: str) -> int:
                 houses_coordinates_robot[1] += 1
             elif k == '<':
                 houses_coordinates_robot[1] -= 1
+
         houses_coordinates_santa: tuple = tuple(houses_coordinates_santa)
         houses_coordinates_robot: tuple = tuple(houses_coordinates_robot)
+
         houses_coordinates_matrix_santa_and_robot.add(houses_coordinates_santa)
         houses_coordinates_matrix_santa_and_robot.add(houses_coordinates_robot)
+
     return len(houses_coordinates_matrix_santa_and_robot)
 
 
