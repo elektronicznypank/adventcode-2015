a5f7364d9d109ec05f62ca6d26a1e0fcb7a58097
elektronicznypank <dizelpauer115@gmail.com>
2023-06-12 00:34:24 +0200

    Reformat code

--- day3/adventcode2015_day3.py
+++ day3/adventcode2015_day3.py
@@ -1,74 +1,67 @@
 with open('input.txt') as houses:
-    houses_directions: str = houses.readline()
-
-# part 1
+    houses_directions: str = houses.readline().replace('\n', '')
 
 
-def count_gifted_houses_by_santa(houses_input: str) -> int:
+def count_gifted_houses_by_santa_and_robot(houses_input: str) -> tuple[int, int]:
 
     houses_coordinates_santa_alone: list = [0, 0]
-    houses_coordinates_matrix_santa: set = {(0, 0)}
-
-    for i in houses_input:
-        houses_coordinates_santa_alone = list(houses_coordinates_santa_alone)
-        if i == '^':
-            houses_coordinates_santa_alone[0] += 1
-        elif i == 'v':
-            houses_coordinates_santa_alone[0] -= 1
-        elif i == '>':
-            houses_coordinates_santa_alone[1] += 1
-        elif i == '<':
-            houses_coordinates_santa_alone[1] -= 1
-
-        houses_coordinates_santa_alone: tuple = tuple(houses_coordinates_santa_alone)
-        houses_coordinates_matrix_santa.add(houses_coordinates_santa_alone)
-
-    return len(houses_coordinates_matrix_santa)
-
-
-# part 2
-
-
-def count_gifted_houses_by_santa_and_robot(houses_input: str) -> int:
-
-    houses_coordinates_santa: list = [0, 0]
-    houses_coordinates_robot: list = [0, 0]
+    houses_coordinates_santa_turn: list = [0, 0]
+    houses_coordinates_robot_turn: list = [0, 0]
+    houses_coordinates_matrix_santa_alone: set = {(0, 0)}
     houses_coordinates_matrix_santa_and_robot: set = {(0, 0)}
 
-    for j, k in enumerate(houses_input):
+    for house_index, sign in enumerate(houses_input):
 
-        houses_coordinates_santa: list = list(houses_coordinates_santa)
-        houses_coordinates_robot: list = list(houses_coordinates_robot)
+        houses_coordinates_santa_alone: list = list(houses_coordinates_santa_alone)
+        houses_coordinates_santa_turn: list = list(houses_coordinates_santa_turn)
+        houses_coordinates_robot_turn: list = list(houses_coordinates_robot_turn)
 
-        if j % 2 == 0:
-            if k == '^':
-                houses_coordinates_santa[0] += 1
-            elif k == 'v':
-                houses_coordinates_santa[0] -= 1
-            elif k == '>':
-                houses_coordinates_santa[1] += 1
-            elif k == '<':
-                houses_coordinates_santa[1] -= 1
+        if house_index % 2 == 0:
+
+            if sign == '^':
+                houses_coordinates_santa_alone[0] += 1
+                houses_coordinates_santa_turn[0] += 1
+            elif sign == 'v':
+                houses_coordinates_santa_alone[0] -= 1
+                houses_coordinates_santa_turn[0] -= 1
+            elif sign == '>':
+                houses_coordinates_santa_alone[1] += 1
+                houses_coordinates_santa_turn[1] += 1
+            elif sign == '<':
+                houses_coordinates_santa_alone[1] -= 1
+                houses_coordinates_santa_turn[1] -= 1
+            else:
+                raise Exception("Houses' input contains illegal characters. Please verify it.")
+
         else:
-            if k == '^':
-                houses_coordinates_robot[0] += 1
-            elif k == 'v':
-                houses_coordinates_robot[0] -= 1
-            elif k == '>':
-                houses_coordinates_robot[1] += 1
-            elif k == '<':
-                houses_coordinates_robot[1] -= 1
 
-        houses_coordinates_santa: tuple = tuple(houses_coordinates_santa)
-        houses_coordinates_robot: tuple = tuple(houses_coordinates_robot)
+            if sign == '^':
+                houses_coordinates_santa_alone[0] += 1
+                houses_coordinates_robot_turn[0] += 1
+            elif sign == 'v':
+                houses_coordinates_santa_alone[0] -= 1
+                houses_coordinates_robot_turn[0] -= 1
+            elif sign == '>':
+                houses_coordinates_santa_alone[1] += 1
+                houses_coordinates_robot_turn[1] += 1
+            elif sign == '<':
+                houses_coordinates_santa_alone[1] -= 1
+                houses_coordinates_robot_turn[1] -= 1
+            else:
+                raise Exception("Houses' input contains illegal characters. Please verify it.")
 
-        houses_coordinates_matrix_santa_and_robot.add(houses_coordinates_santa)
-        houses_coordinates_matrix_santa_and_robot.add(houses_coordinates_robot)
+        houses_coordinates_santa_alone: tuple = tuple(houses_coordinates_santa_alone)
+        houses_coordinates_santa_turn: tuple = tuple(houses_coordinates_santa_turn)
+        houses_coordinates_robot_turn: tuple = tuple(houses_coordinates_robot_turn)
 
-    return len(houses_coordinates_matrix_santa_and_robot)
+        houses_coordinates_matrix_santa_alone.add(houses_coordinates_santa_alone)
+        houses_coordinates_matrix_santa_and_robot.add(houses_coordinates_santa_turn)
+        houses_coordinates_matrix_santa_and_robot.add(houses_coordinates_robot_turn)
+
+    return len(houses_coordinates_matrix_santa_alone), len(houses_coordinates_matrix_santa_and_robot)
 
 
 if __name__ == '__main__':
-    print(f'Santa left at least one present in {count_gifted_houses_by_santa(houses_directions)} houses.')
-    print(f'Santa and robot left at least one present in '
-          f'{count_gifted_houses_by_santa_and_robot(houses_directions)} houses.')
+    gifted_houses_santa_alone, gifted_houses_santa_and_robot = count_gifted_houses_by_santa_and_robot(houses_directions)
+    print(f'Santa left at least one present in {gifted_houses_santa_alone} houses.')
+    print(f'Santa and robot left at least one present in {gifted_houses_santa_and_robot} houses.')
---
+++ day3/test_adventcode2015_day3.py
@@ -0,0 +1,12 @@
+from adventcode2015_day3 import count_gifted_houses_by_santa_and_robot
+
+test_cases: list[dict[str, int, int]] = [
+    # {'input': 'gfhrcops',
+]
+
+
+# def test_day3_function():
+
+
+if __name__ == '__main__':
+    test_day3_function()
